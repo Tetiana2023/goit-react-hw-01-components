b6931accfd4e54dadffe25d671aac3a02a99f2f6
Tetiana2023 <drofaj18@gmail.com>
2023-03-02 13:24:57 +0200

    userProfile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Profile } from "../components/UserProfile/Profile";
+import user from "../components/UserProfile/user.json"
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+    <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />
+             
     </div>
   );
 };
+// style={{
+//   height: '100vh',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+//   fontSize: 40,
+//   color: '#010101'
